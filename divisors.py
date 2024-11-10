9932418d7d2c5f82e830a516485a3130c9d161a2
yunho05 <128762726+yunho05@users.noreply.github.com>
2024-11-10 15:55:08 +0900

    Add files via upload

--- divisors.py
+++ divisors.py
@@ -1,1 +1,9 @@
-print("hello world")
+number = int(input("약수를 구할 숫자를 입력하세요: "))
+
+divisors = []
+
+for i in range(1, number + 1):
+    if number % i == 0:
+        divisors.append(i)
+
+print(f"{number}의 약수: {divisors}")
